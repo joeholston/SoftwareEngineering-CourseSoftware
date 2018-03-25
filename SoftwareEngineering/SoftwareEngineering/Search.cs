3020dfc6dd92c1c44b4e0e4c1d4efc7f46cdad05
WyattHough <35705839+WyattHough@users.noreply.github.com>
2018-03-24 20:43:08 -0400

    search edit

--- SoftwareEngineering/SoftwareEngineering/Search.cs
+++ SoftwareEngineering/SoftwareEngineering/Search.cs
@@ -8,5 +8,19 @@ namespace SoftwareEngineering
 {
     class Search
     {
+        private List<Course> searchCourses = new List<Course>();
+
+        private List<Course> courseDB = new List<Course>();
+
+        public Search(string input)
+        {
+            for (int i = 0; i < courseDB.Count; i++)
+            {
+                if (input == courseDB[i].courseCode | input == courseDB[i].shortTitle | input == courseDB[i].LongTitle)
+                {
+                    searchCourses.Add(courseDB[i]);
+                }
+            }
+        }
     }
 }
