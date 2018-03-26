afae40b6aff7d0d6c21bb2656b259cf606ed2408
WyattHough <35705839+WyattHough@users.noreply.github.com>
2018-03-25 20:31:58 -0400

    search edit

--- SoftwareEngineering/SoftwareEngineering/Search.cs
+++ SoftwareEngineering/SoftwareEngineering/Search.cs
@@ -8,10 +8,14 @@ namespace SoftwareEngineering
 {
     class Search
     {
+        //create a list to hold searched courses
         private List<Course> searchCourses = new List<Course>();
 
-        private List<Course> courseDB = new List<Course>();
+        //create a list of the course database
+        private List<Course> courseDB = BuildDB.Instance.database;
 
+        //search the course database list for matches to the input string
+        //      searches for a direct match of course code, short title, and long title
         public Search(string input)
         {
             for (int i = 0; i < courseDB.Count; i++)
