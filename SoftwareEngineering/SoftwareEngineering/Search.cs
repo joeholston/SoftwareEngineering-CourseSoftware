cf9ea9cbd288330b12bf7e8280525bdf92d3517e
WyattHough <35705839+WyattHough@users.noreply.github.com>
2018-04-23 17:16:07 -0400

    Added course name search and fixed errors
    results will now work when the search contains a mix of upper and lower case letters

--- SoftwareEngineering/SoftwareEngineering/Search.cs
+++ SoftwareEngineering/SoftwareEngineering/Search.cs
@@ -8,21 +8,39 @@ namespace SoftwareEngineering
 {
     class Search
     {
-        //create a list to hold searched courses
+        //create a list to hold searched for courses
         public List<Course> searchCourses = new List<Course>();
 
         //create a list of the course database
         private List<Course> courseDB = BuildDB.Instance.courseDatabase;
 
+        //variable decide whether user is searching by Course Code or Course Name or Professor
+        // 0 = Course Code
+        // 1 = Course Name
+        public int searchSelection = 0;
+
         //search the course database list for matches to the input string
         public void search(string input)
         {
             searchCourses.Clear();
+
             for (int i = 0; i < courseDB.Count; i++)
             {
-                if (courseDB[i].courseCode.StartsWith(input) | courseDB[i].courseCode.ToLower().StartsWith(input))
+                //decides whether to search for courseCode or courseName based on searchSelection variable
+                if (searchSelection == 0)
                 {
-                    searchCourses.Add(courseDB[i]);
+                    if (courseDB[i].courseCode.StartsWith(input.ToUpper()))
+                    {
+                        searchCourses.Add(courseDB[i]);
+                    }
+                }
+                else if (searchSelection == 1)
+                {
+                    //It will search both the short or long title of the courses
+                    if (courseDB[i].shortTitle.StartsWith(input.ToUpper()) | courseDB[i].LongTitle.StartsWith(input.ToUpper()))
+                    {
+                        searchCourses.Add(courseDB[i]);
+                    }
                 }
             }
         }
